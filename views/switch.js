5d41ddbd03f4de95aadb18bc3d09df1fb91fe72a
M-Ashrey <m.ashrey122@gmail.com>
2023-03-30 04:42:27 +0200

    minor js script refactor

--- views/switch.js
+++ views/switch.js
@@ -24,68 +24,63 @@ let setSpec = function() {
   document.getElementById("spec").value = `${length} x ${width} x ${height}`;
 }
 
+const prodTypes = {
+  dvd: `<label for="size">Size (MB) *</label>
+          <input name="spec" required 
+          type="number" id="size" 
+          min="1" max="1000000000" 
+          autocomplete="off"
+          <span id="description">Please, provide size in MB format.</span>`,
+
+  furniture: `
+          <label for="height">Height (cm) *</label>
+          <input name="height" required 
+          type="number" id="height"
+          min="1" max="100000" 
+          autocomplete="off">
+          <label for="width">Width (cm) *</label>
+          <input name="width" required 
+          type="number" id="width" 
+          min="1" max="100000" 
+          autocomplete="off">
+          
+          <label for="length">Length (cm) *</label>
+          <input name="length" required 
+          type="number" id="length" 
+          min="1" max="100000" 
+          autocomplete="off">
+          <span id="description">Please, provide dimensions in HxWxL format.</span>
+          
+          <input hidden name="spec" required 
+          type="text" id="spec"
+          autocomplete="off">
+          `,
+
+  book: `
+          <label for="weight">Weight (kg) *</label>
+          <input name="spec" required
+          type="text" id="weight" 
+          minlength="1" maxlength="10" 
+          autocomplete="off">
+          <span id="description">Please, provide weight in KG format.</span>`
+
+}
+
 let switchFunc = function () {
-    const dvd = `
-      <label for="size">Size (MB) *</label>
-      <input name="spec" required 
-      type="number" id="size" 
-      min="1" max="1000000000" 
-      autocomplete="off"
-      <span id="description">Please, provide size in MB format.</span>`;
+    
 
-    const furniture = `
-      <label for="height">Height (cm) *</label>
-      <input name="height" required 
-      type="number" id="height"
-      min="1" max="100000" 
-      autocomplete="off">
-      <label for="width">Width (cm) *</label>
-      <input name="width" required 
-      type="number" id="width" 
-      min="1" max="100000" 
-      autocomplete="off">
-      
-      <label for="length">Length (cm) *</label>
-      <input name="length" required 
-      type="number" id="length" 
-      min="1" max="100000" 
-      autocomplete="off">
-      <span id="description">Please, provide dimensions in HxWxL format.</span>
-      
-      <input hidden name="spec" required 
-      type="text" id="spec"
-      autocomplete="off">
-      `;
-      
-
-    const book = `
-      <label for="weight">Weight (kg) *</label>
-      <input name="spec" required
-      type="text" id="weight" 
-      minlength="1" maxlength="10" 
-      autocomplete="off">
-      <span id="description">Please, provide weight in KG format.</span>`;
-
-    let type = productType.value;
-    type = type.toLowerCase();
+    let type = typeSwitch.value.toLowerCase();
     let content = document.getElementById("cont");
     
-    switch (type) {
-      case "dvd":
-        content.innerHTML = dvd;
-        break;
-      case "furniture":
-        content.innerHTML = furniture;
-        document.getElementById("length").addEventListener('input', setSpec)
-        document.getElementById("width").addEventListener('input', setSpec)
-        document.getElementById("height").addEventListener('input', setSpec)
-        break;
-      case "book":
-        content.innerHTML = book;
-        break;
-      default:
-        content.innerHTML = "";
-    }    
+    content.innerHTML = prodTypes[type];
+
+    if (type == 'furniture') {
+      document.getElementById("length").addEventListener('input', setSpec);
+      document.getElementById("width").addEventListener('input', setSpec);
+      document.getElementById("height").addEventListener('input', setSpec);
+    }
 }
 
 typeSwitch.addEventListener('input', switchFunc)
+
+
